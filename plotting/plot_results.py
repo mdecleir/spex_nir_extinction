9a157f40f822e384807116ec40dcfec697b61430
mdecleir <mdecleir@stsci.edu>
2021-03-30 10:55:45 -0400

    Create tables

--- plotting/plot_results.py
+++ plotting/plot_results.py
@@ -1,10 +1,170 @@
-# This script plots the results of the fitting
+# This script creates tables and plots of the results of the fitting for the NIR extinction paper (Decleir et al. 2021).
 
 import numpy as np
 from matplotlib import pyplot as plt
 from scipy import stats
+from astropy.table import Table
+import pandas as pd
 
 from measure_extinction.extdata import ExtData
+
+
+def table_results(inpath, outpath, starpair_list, flagged):
+    """
+    Create tables with the fitting results:
+        - One to save as a text file
+        - One in the aastex format
+    Parameters
+    ----------
+    inpath : string
+        Path to the input data files
+
+    outpath : string
+        Path to save the output files
+
+    starpair_list : list of strings
+        List of star pairs to include in the tables, in the format "reddenedstarname_comparisonstarname" (no spaces)
+
+    flagged : list of strings
+        List of star pairs to exclude from the tables, in the format "reddenedstarname_comparisonstarname" (no spaces)
+
+    Returns
+    -------
+    Tables with fitting results
+    """
+    # create empty tables
+    table_txt = Table(
+        names=(
+            "reddened",
+            "comparison",
+            "amplitude",
+            "ampl. left unc.",
+            "ampl. right unc.",
+            "alpha",
+            "alpha left unc.",
+            "alpha right unc.",
+            "AV",
+            "AV left unc.",
+            "AV right unc.",
+            "EBV",
+            "EBV left unc.",
+            "EBV right unc.",
+            "RV",
+            "RV left unc.",
+            "RV right unc.",
+        ),
+        dtype=(
+            "str",
+            "str",
+            "float64",
+            "float64",
+            "float64",
+            "float64",
+            "float64",
+            "float64",
+            "float64",
+            "float64",
+            "float64",
+            "float64",
+            "float64",
+            "float64",
+            "float64",
+            "float64",
+            "float64",
+        ),
+    )
+    table_lat = Table(
+        names=(
+            "reddened",
+            "comparison",
+            "amplitude",
+            r"$\alpha$",
+            "A(V)",
+            "E(B-V)",
+            "R(V)",
+        ),
+        dtype=("str", "str", "str", "str", "str", "str", "str"),
+    )
+
+    # retrieve the fitting results for all stars
+    for starpair in starpair_list:
+        if starpair in flagged:
+            continue
+        extdata = ExtData("%s%s_ext.fits" % (inpath, starpair.lower()))
+        table_txt.add_row(
+            (
+                starpair.split("_")[0],
+                starpair.split("_")[1],
+                extdata.model["params"][0].value,
+                extdata.model["params"][0].unc_minus,
+                extdata.model["params"][0].unc_plus,
+                extdata.model["params"][2].value,
+                extdata.model["params"][2].unc_minus,
+                extdata.model["params"][2].unc_plus,
+                extdata.columns["AV"][0],
+                extdata.columns["AV"][1],
+                extdata.columns["AV"][2],
+                extdata.columns["EBV"][0],
+                extdata.columns["EBV"][1],
+                extdata.columns["EBV"][2],
+                extdata.columns["RV"][0],
+                extdata.columns["RV"][1],
+                extdata.columns["RV"][2],
+            )
+        )
+        table_lat.add_row(
+            (
+                starpair.split("_")[0],
+                starpair.split("_")[1],
+                "${:.2f}".format(extdata.model["params"][0].value)
+                + "_{-"
+                + "{:.3f}".format(extdata.model["params"][0].unc_minus)
+                + "}^{+"
+                + "{:.3f}".format(extdata.model["params"][0].unc_plus)
+                + "}$",
+                "${:.2f}".format(extdata.model["params"][2].value)
+                + "_{-"
+                + "{:.3f}".format(extdata.model["params"][2].unc_minus)
+                + "}^{+"
+                + "{:.3f}".format(extdata.model["params"][2].unc_plus)
+                + "}$",
+                "${:.2f}".format(extdata.columns["AV"][0])
+                + "_{-"
+                + "{:.3f}".format(extdata.columns["AV"][1])
+                + "}^{+"
+                + "{:.3f}".format(extdata.columns["AV"][2])
+                + "}$",
+                "${:.2f}".format(extdata.columns["EBV"][0])
+                + "_{-"
+                + "{:.3f}".format(extdata.columns["EBV"][1])
+                + "}^{+"
+                + "{:.3f}".format(extdata.columns["EBV"][2])
+                + "}$",
+                "${:.2f}".format(extdata.columns["RV"][0])
+                + "_{-"
+                + "{:.3f}".format(extdata.columns["RV"][1])
+                + "}^{+"
+                + "{:.3f}".format(extdata.columns["RV"][2])
+                + "}$",
+            )
+        )
+
+    # write the tables to files
+    table_txt.write(
+        outpath + "fitting_results.dat",
+        format="ascii.commented_header",
+        overwrite=True,
+    )
+
+    table_lat.write(
+        outpath + "fitting_results.tex",
+        format="aastex",
+        latexdict={
+            "tabletype": "deluxetable*",
+            "caption": r"MCMC fitting results for the extinction curves: the amplitude and index $\alpha$ of the powerlaw, and A(V) are directly obtained from the fitting, while E(B-V) is obtained from the observations, and R(V) is calculated as R(V)=A(V)/E(B-V).",
+        },
+        overwrite=True,
+    )
 
 
 def plot_params(ax, x, y, x_err=None, y_err=None, flagged=None):
@@ -33,7 +193,7 @@ def plot_params(ax, x, y, x_err=None, y_err=None, flagged=None):
         )
 
 
-def plot_param_triangle(starpair_list, flagged):
+def plot_param_triangle(inpath, outpath, starpair_list, flagged):
     amplitudes, amp_l, amp_u, alphas, alpha_l, alpha_u, AVs, AV_l, AV_u, RVs = (
         np.zeros(len(starpair_list)) for i in range(10)
     )
@@ -41,7 +201,7 @@ def plot_param_triangle(starpair_list, flagged):
 
     # retrieve the fitting results
     for i, starpair in enumerate(starpair_list):
-        extdata = ExtData("%s%s_ext.fits" % (path, starpair.lower()))
+        extdata = ExtData("%s%s_ext.fits" % (inpath, starpair.lower()))
         amplitudes[i] = extdata.model["params"][0].value
         amp_l[i] = extdata.model["params"][0].unc_minus
         amp_u[i] = extdata.model["params"][0].unc_plus
@@ -89,7 +249,7 @@ def plot_param_triangle(starpair_list, flagged):
     ax[0, 2].axis("off")
     ax[1, 2].axis("off")
     plt.subplots_adjust(wspace=0, hspace=0)
-    plt.savefig("Figures/params.pdf", bbox_inches="tight")
+    plt.savefig(outpath + "params.pdf", bbox_inches="tight")
 
 
 def plot_rv_dep(inpath, outpath, starpair_list, flagged):
@@ -124,7 +284,7 @@ def plot_rv_dep(inpath, outpath, starpair_list, flagged):
     # plot A(lambda)/A(V) vs. R(V) at certain wavelengths
     fig, ax = plt.subplots(len(wave_list), figsize=(7, len(wave_list) * 4), sharex=True)
     for j, wave in enumerate(wave_list):
-        ax[j].scatter(RVs, alavs[j])
+        ax[j].scatter(1 / RVs, alavs[j])
         rho, p = stats.spearmanr(RVs, alavs[j], nan_policy="omit")
         ax[j].text(
             0.95,
@@ -139,23 +299,33 @@ def plot_rv_dep(inpath, outpath, starpair_list, flagged):
     # finalize the plot
     plt.xlabel("R(V)")
     plt.subplots_adjust(hspace=0)
-    plt.savefig(outpath + "RV_dep.pdf", bbox_inches="tight")
+    plt.savefig(outpath + "RV_dep2.pdf", bbox_inches="tight")
+
+
+def compare_AV_lit():
+    # Obtain AVs from the literature
+    CCM89 = pd.read_table(
+        "/Users/mdecleir/spex_nir_extinction/data/Cardelli+89_tab1.dat", sep="\s"
+    )
+    CCM89["AV"] = CCM89["RV"] * CCM89["E(B-V)"]
+    print(CCM89)
 
 
 if __name__ == "__main__":
     # define the input and output path and the names of the star pairs in the format "reddenedstarname_comparisonstarname"
     inpath = "/Users/mdecleir/Documents/NIR_ext/Data/"
-    outpath = "/Users/mdecleir/spex_nir_extinction/Figures/"
+    plot_path = "/Users/mdecleir/spex_nir_extinction/Figures/"
+    table_path = "/Users/mdecleir/spex_nir_extinction/Tables/"
     starpair_list = [
-        "HD017505_HD214680",
         "BD+56d524_HD034816",
         "HD013338_HD031726",
         "HD014250_HD042560",
         "HD014422_HD214680",
         "HD014956_HD188209",
+        "HD017505_HD214680",
         "HD029309_HD042560",
         "HD029647_HD042560",
-        # "HD034921_HD214680",
+        "HD034921_HD214680",
         "HD037020_HD034816",
         "HD037022_HD034816",
         "HD037023_HD034816",
@@ -185,6 +355,10 @@ if __name__ == "__main__":
         "HD014422_HD214680",
     ]
 
-    # create plot
-    plot_param_triangle(starpair_list, flagged)
-    plot_rv_dep(inpath, outpath, starpair_list, flagged)
+    # create tables
+    table_results(inpath, table_path, starpair_list, flagged)
+
+    # create plots
+    # plot_param_triangle(inpath, outpath, starpair_list, flagged)
+    # plot_rv_dep(inpath, outpath, starpair_list, flagged)
+    # compare_AV_lit()
