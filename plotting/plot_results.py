f3a1ea95a1f2622ddba155550b194bbee0a69f88
mdecleir <mdecleir@stsci.edu>
2021-04-06 11:52:11 -0400

    Added A(V) comparison to literature

--- plotting/plot_results.py
+++ plotting/plot_results.py
@@ -5,6 +5,8 @@ from matplotlib import pyplot as plt
 from scipy import stats
 from astropy.table import Table
 import pandas as pd
+from collections import Counter
+from matplotlib.lines import Line2D
 
 from measure_extinction.extdata import ExtData
 
@@ -170,6 +172,7 @@ def table_results(inpath, outpath, starpair_list, flagged):
 
 def plot_params(ax, x, y, x_err=None, y_err=None, flagged=None):
     ax.errorbar(x, y, xerr=x_err, yerr=y_err, fmt=".k", zorder=0)
+    print(x)
     rho, p = stats.spearmanr(x, y)
     ax.text(
         0.95,
@@ -253,18 +256,97 @@ def plot_param_triangle(inpath, outpath, starpair_list, flagged):
     plt.savefig(outpath + "params.pdf", bbox_inches="tight")
 
 
-def compare_AV_lit():
-    # Obtain AVs from the literature
+def compare_AV_lit(ext_path, lit_path, starpair_list):
+    """
+    Function to create a plot with the comparison of calculated A(V) values and values from the literature
+
+    Parameters
+    ----------
+    ext_path : string
+        Path to the extinction data
+
+    lit_path : string
+        Path to the literature data
+
+    starpair_list : list of strings
+        List of star pairs to include in the plot, in the format "reddenedstarname_comparisonstarname" (no spaces)
+    """
+    # Obtain A(V) values from the literature
+    # Cardelli, Clayton and Mathis 1989
     CCM89 = pd.read_table(
-        "/Users/mdecleir/spex_nir_extinction/data/Cardelli+89_tab1.dat", sep="\s"
+        lit_path + "Cardelli+89_tab1.dat", sep="\s+", index_col="Star"
     )
-    CCM89["AV"] = CCM89["RV"] * CCM89["E(B-V)"]
-    print(CCM89)
+    CCM89["A(V)"] = CCM89["R(V)"] * CCM89["E(B-V)"]
+
+    # Valencic, Clayton and Gordon 2004
+    VCG04 = Table.read(
+        lit_path + "Valencic+04_tab4.dat", format="ascii.cds"
+    ).to_pandas()
+    VCG04.loc[:355, "Name"] = "HD" + VCG04.loc[:355, "Name"].str.replace(
+        "HD ", ""
+    ).str.zfill(6)
+    VCG04.loc[355:392, "Name"] = VCG04.loc[355:392, "Name"].str.replace(" ", "d")
+    VCG04.set_index("Name", inplace=True)
+
+    # Fitzpatrick et al. 2019
+
+    # Gordon et al. 2021
+    G21 = pd.read_table(lit_path + "Gordon+21_tab5.dat", sep="\s+", index_col="Star")
+
+    # plot the literature A(V) values against the calculated values
+    fig, ax = plt.subplots(figsize=(6, 6))
+    for starpair in starpair_list:
+        extdata = ExtData("%s%s_ext.fits" % (ext_path, starpair.lower()))
+        red_star = starpair.split("_")[0]
+        if red_star in CCM89.index:
+            ax.scatter(
+                extdata.columns["AV"][0],
+                CCM89.loc[red_star, "A(V)"],
+                color="tab:blue",
+            )
+        if red_star in VCG04.index:
+            ax.scatter(
+                extdata.columns["AV"][0],
+                VCG04.loc[red_star, "A(V)"],
+                color="tab:orange",
+                marker="d",
+            )
+        if red_star.lower() in G21.index:
+            ax.scatter(
+                extdata.columns["AV"][0],
+                G21.loc[red_star.lower(), "A(V)"],
+                color="tab:green",
+                marker="P",
+            )
+        ax.errorbar(
+            extdata.columns["AV"][0],
+            extdata.columns["AV"][0],
+            # yerr=([extdata.columns["AV"][1]], [extdata.columns["AV"][2]]),
+            yerr=([0.2], [0.2]),
+            color="k",
+            lw=0.5,
+            alpha=0.6,
+        )
+
+    handle1 = Line2D([], [], lw=0, color="tab:blue", marker="o")
+    handle2 = Line2D([], [], lw=0, color="tab:orange", marker="d")
+    handle3 = Line2D([], [], lw=0, color="tab:green", marker="P")
+    labels = ["Cardelli+1989", "Valencic+2004", "Gordon+2021"]
+    handles = [handle1, handle2, handle3]
+    ax.plot([1, 5.7], [1, 5.7], color="k", ls="--")
+    ax.set_xlim(0.9, 5.8)
+    ax.set_ylim(0.9, 5.8)
+    ax.set_aspect("equal", adjustable="box")
+    plt.legend(handles, labels, fontsize=fs * 0.8)
+    plt.xlabel("A(V) from this work")
+    plt.ylabel("A(V) from the literature")
+    plt.savefig(plot_path + "AV_comparison.pdf", bbox_inches="tight")
 
 
 if __name__ == "__main__":
     # define the input and output path and the names of the star pairs in the format "reddenedstarname_comparisonstarname"
     inpath = "/Users/mdecleir/Documents/NIR_ext/Data/"
+    data_path = "/Users/mdecleir/spex_nir_extinction/data/"
     plot_path = "/Users/mdecleir/spex_nir_extinction/Figures/"
     table_path = "/Users/mdecleir/spex_nir_extinction/Tables/"
     starpair_list = [
@@ -276,7 +358,7 @@ if __name__ == "__main__":
         "HD017505_HD214680",
         "HD029309_HD042560",
         "HD029647_HD042560",
-        "HD034921_HD214680",
+        # "HD034921_HD214680",
         "HD037020_HD034816",
         "HD037022_HD034816",
         "HD037023_HD034816",
@@ -306,10 +388,20 @@ if __name__ == "__main__":
         "HD014422_HD214680",
     ]
 
+    # subtract the flagged stars from the star pair list
+    good_stars = list((Counter(starpair_list) - Counter(flagged)).elements())
+
+    # settings for the plotting
+    fs = 18
+    plt.rc("font", size=fs)
+    plt.rc("xtick", direction="in", labelsize=fs * 0.8)
+    plt.rc("ytick", direction="in", labelsize=fs * 0.8)
+    plt.rc("xtick.major", width=1, size=8)
+    plt.rc("ytick.major", width=1, size=8)
+
     # create tables
-    table_results(inpath, table_path, starpair_list, flagged)
+    # table_results(inpath, table_path, starpair_list, flagged)
 
     # create plots
-    # plot_param_triangle(inpath, outpath, starpair_list, flagged)
-    # plot_rv_dep(inpath, outpath, starpair_list, flagged)
-    # compare_AV_lit()
+    # plot_param_triangle(inpath, plot_path, starpair_list, flagged)
+    compare_AV_lit(inpath, data_path, good_stars)
