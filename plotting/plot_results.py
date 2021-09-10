3a69c739c8197d7ac74768b573b4de7598cdaf0a
mdecleir <mdecleir@stsci.edu>
2021-09-10 17:42:11 -0400

    Added average curve to plots and table

--- plotting/plot_results.py
+++ plotting/plot_results.py
@@ -7,6 +7,7 @@ from astropy.table import Table
 import pandas as pd
 from collections import Counter
 from matplotlib.lines import Line2D
+import astropy.units as u
 
 from measure_extinction.extdata import ExtData
 
@@ -118,7 +119,7 @@ def table_results(inpath, outpath, diffuse, dense):
             (
                 starpair.split("_")[0],
                 starpair.split("_")[1],
-                "{:.2f}".format(extdata.model["params"][0].value)
+                "{:.3f}".format(extdata.model["params"][0].value)
                 + "_{-"
                 + "{:.3f}".format(extdata.model["params"][0].unc_minus)
                 + "}^{+"
@@ -126,30 +127,60 @@ def table_results(inpath, outpath, diffuse, dense):
                 + "}",
                 "{:.2f}".format(extdata.model["params"][2].value)
                 + "_{-"
-                + "{:.3f}".format(extdata.model["params"][2].unc_minus)
+                + "{:.2f}".format(extdata.model["params"][2].unc_minus)
                 + "}^{+"
-                + "{:.3f}".format(extdata.model["params"][2].unc_plus)
+                + "{:.2f}".format(extdata.model["params"][2].unc_plus)
                 + "}",
                 "{:.2f}".format(extdata.columns["AV"][0])
                 + "_{-"
-                + "{:.3f}".format(extdata.columns["AV"][1])
+                + "{:.2f}".format(extdata.columns["AV"][1])
                 + "}^{+"
-                + "{:.3f}".format(extdata.columns["AV"][2])
+                + "{:.2f}".format(extdata.columns["AV"][2])
                 + "}",
                 "{:.2f}".format(extdata.columns["EBV"][0])
                 + "_{-"
-                + "{:.3f}".format(extdata.columns["EBV"][1])
+                + "{:.2f}".format(extdata.columns["EBV"][1])
                 + "}^{+"
-                + "{:.3f}".format(extdata.columns["EBV"][2])
+                + "{:.2f}".format(extdata.columns["EBV"][2])
                 + "}",
                 "{:.2f}".format(extdata.columns["RV"][0])
                 + "_{-"
-                + "{:.3f}".format(extdata.columns["RV"][1])
+                + "{:.2f}".format(extdata.columns["RV"][1])
                 + "}^{+"
-                + "{:.3f}".format(extdata.columns["RV"][2])
+                + "{:.2f}".format(extdata.columns["RV"][2])
                 + "}",
             )
         )
+
+    # add the average diffuse extinction fitting results
+    average = ExtData(inpath + "average_ext.fits")
+    (abav,) = average.exts["BAND"][average.waves["BAND"] == 0.438 * u.micron]
+    (abav_unc,) = average.uncs["BAND"][average.waves["BAND"] == 0.438 * u.micron]
+    rel_unc = abav_unc / abav
+    ave_RV = 1 / (abav - 1)
+    ave_RV_unc = ave_RV * rel_unc
+
+    table_lat.add_row(
+        (
+            "average",
+            "diffuse",
+            "{:.3f}".format(average.model["params"][0].value)
+            + "_{-"
+            + "{:.3f}".format(average.model["params"][0].unc_minus)
+            + "}^{+"
+            + "{:.3f}".format(average.model["params"][0].unc_plus)
+            + "}",
+            "{:.2f}".format(average.model["params"][2].value)
+            + "_{-"
+            + "{:.2f}".format(average.model["params"][2].unc_minus)
+            + "}^{+"
+            + "{:.2f}".format(average.model["params"][2].unc_plus)
+            + "}",
+            "",
+            "",
+            "{:.2f}".format(ave_RV) + r"$\pm$" + "{:.2f}".format(ave_RV_unc),
+        )
+    )
 
     # write the tables to files
     table_txt.write(
@@ -164,7 +195,7 @@ def table_results(inpath, outpath, diffuse, dense):
         col_align="ll|CCCCC",
         latexdict={
             "tabletype": "deluxetable*",
-            "caption": r"MCMC fitting results for the 13 diffuse extinction curves: the amplitude $S$ and index $\alpha$ of the power law, and $A(V)$ are directly obtained from the fitting, while $E(B-V)$ is obtained from the observations, and $R(V)$ is calculated as $R(V)=A(V)/E(B-V)$. \label{tab:fit_results_diff}",
+            "caption": r"MCMC fitting results for the 13 diffuse extinction curves and the average diffuse extinction curve. \label{tab:fit_results_diff}",
         },
         overwrite=True,
     )
@@ -300,11 +331,12 @@ def table_results(inpath, outpath, diffuse, dense):
                 extdata.columns["RV"][2],
             )
         )
+
         table_lat.add_row(
             (
                 starpair.split("_")[0],
                 starpair.split("_")[1],
-                "{:.2f}".format(extdata.model["params"][0].value)
+                "{:.3f}".format(extdata.model["params"][0].value)
                 + "_{-"
                 + "{:.3f}".format(extdata.model["params"][0].unc_minus)
                 + "}^{+"
@@ -312,17 +344,17 @@ def table_results(inpath, outpath, diffuse, dense):
                 + "}",
                 "{:.2f}".format(extdata.model["params"][2].value)
                 + "_{-"
-                + "{:.3f}".format(extdata.model["params"][2].unc_minus)
+                + "{:.2f}".format(extdata.model["params"][2].unc_minus)
                 + "}^{+"
-                + "{:.3f}".format(extdata.model["params"][2].unc_plus)
+                + "{:.2f}".format(extdata.model["params"][2].unc_plus)
                 + "}",
-                "{:.2f}".format(extdata.model["params"][3].value)
+                "{:.3f}".format(extdata.model["params"][3].value)
                 + "_{-"
                 + "{:.3f}".format(extdata.model["params"][3].unc_minus)
                 + "}^{+"
                 + "{:.3f}".format(extdata.model["params"][3].unc_plus)
                 + "}",
-                "{:.2f}".format(extdata.model["params"][4].value)
+                "{:.3f}".format(extdata.model["params"][4].value)
                 + "_{-"
                 + "{:.3f}".format(extdata.model["params"][4].unc_minus)
                 + "}^{+"
@@ -330,33 +362,33 @@ def table_results(inpath, outpath, diffuse, dense):
                 + "}",
                 "{:.2f}".format(extdata.model["params"][5].value)
                 + "_{-"
-                + "{:.3f}".format(extdata.model["params"][5].unc_minus)
+                + "{:.2f}".format(extdata.model["params"][5].unc_minus)
                 + "}^{+"
-                + "{:.3f}".format(extdata.model["params"][5].unc_plus)
+                + "{:.2f}".format(extdata.model["params"][5].unc_plus)
                 + "}",
                 "{:.2f}".format(extdata.model["params"][6].value)
                 + "_{-"
-                + "{:.3f}".format(extdata.model["params"][6].unc_minus)
+                + "{:.2f}".format(extdata.model["params"][6].unc_minus)
                 + "}^{+"
-                + "{:.3f}".format(extdata.model["params"][6].unc_plus)
+                + "{:.2f}".format(extdata.model["params"][6].unc_plus)
                 + "}",
                 "{:.2f}".format(extdata.columns["AV"][0])
                 + "_{-"
-                + "{:.3f}".format(extdata.columns["AV"][1])
+                + "{:.2f}".format(extdata.columns["AV"][1])
                 + "}^{+"
-                + "{:.3f}".format(extdata.columns["AV"][2])
+                + "{:.2f}".format(extdata.columns["AV"][2])
                 + "}",
                 "{:.2f}".format(extdata.columns["EBV"][0])
                 + "_{-"
-                + "{:.3f}".format(extdata.columns["EBV"][1])
+                + "{:.2f}".format(extdata.columns["EBV"][1])
                 + "}^{+"
-                + "{:.3f}".format(extdata.columns["EBV"][2])
+                + "{:.2f}".format(extdata.columns["EBV"][2])
                 + "}",
                 "{:.2f}".format(extdata.columns["RV"][0])
                 + "_{-"
-                + "{:.3f}".format(extdata.columns["RV"][1])
+                + "{:.2f}".format(extdata.columns["RV"][1])
                 + "}^{+"
-                + "{:.3f}".format(extdata.columns["RV"][2])
+                + "{:.2f}".format(extdata.columns["RV"][2])
                 + "}",
             )
         )
@@ -374,7 +406,7 @@ def table_results(inpath, outpath, diffuse, dense):
         col_align="ll|CCCCCCCCC",
         latexdict={
             "tabletype": "deluxetable*",
-            "caption": r"MCMC fitting results for the 2 dense extinction curves: the amplitude $S$ and index $\alpha$ of the power law, the strength $B$, central wavelength $\lambda_0$, width $\gamma_0$ and asymmetry $a$ of the Drude profile, and $A(V)$ are directly obtained from the fitting, while $E(B-V)$ is obtained from the observations, and $R(V)$ is calculated as $R(V)=A(V)/E(B-V)$. \label{tab:fit_results_dense}",
+            "caption": r"MCMC fitting results for the 2 dense extinction curves. \label{tab:fit_results_dense}",
         },
         overwrite=True,
     )
@@ -396,7 +428,7 @@ def plot_params(
         yerr=(y_err[0][dense], y_err[1][dense]),
         fmt="s",
         color="magenta",
-        markersize=4,
+        markersize=5,
         zorder=0,
         label="dense",
     )
@@ -406,16 +438,16 @@ def plot_params(
         xerr=(x_err[0][~dense], x_err[1][~dense]),
         yerr=(y_err[0][~dense], y_err[1][~dense]),
         fmt="ok",
-        markersize=4,
+        markersize=5,
         zorder=0,
         label="diffuse",
     )
     rho, p = stats.spearmanr(x, y)
     ax.text(
-        0.95,
-        0.9,
+        0.94,
+        0.88,
         r"$\rho =$" + "{:1.2f}".format(rho),
-        fontsize=12,
+        fontsize=0.7 * fs,
         horizontalalignment="right",
         transform=ax.transAxes,
     )
@@ -494,6 +526,59 @@ def plot_param_triangle(inpath, outpath, diffuse, dense):
     # plot R(V) vs. A(V)
     plot_params(ax[2, 2], dense_bool, AVs, RVs, (AV_min, AV_plus), (RV_min, RV_plus))
 
+    # add the average diffuse extinction fitting results
+    average = ExtData(inpath + "average_ext.fits")
+    amp = average.model["params"][0].value
+    amp_min = average.model["params"][0].unc_minus
+    amp_plus = average.model["params"][0].unc_plus
+    alpha = average.model["params"][2].value
+    alpha_min = average.model["params"][2].unc_minus
+    alpha_plus = average.model["params"][2].unc_plus
+    (abav,) = average.exts["BAND"][average.waves["BAND"] == 0.438 * u.micron]
+    (abav_unc,) = average.uncs["BAND"][average.waves["BAND"] == 0.438 * u.micron]
+    rel_unc = abav_unc / abav
+    ave_RV = 1 / (abav - 1)
+    ave_RV_unc = ave_RV * rel_unc
+
+    ax[0, 0].errorbar(
+        amp,
+        alpha,
+        xerr=[[amp_min], [amp_plus]],
+        yerr=([alpha_min], [alpha_plus]),
+        fmt="*",
+        markersize=14,
+        zorder=0,
+        alpha=0.8,
+        color="crimson",
+        label="average diffuse",
+    )
+
+    ax[2, 0].errorbar(
+        amp,
+        ave_RV,
+        xerr=[[amp_min], [amp_plus]],
+        yerr=ave_RV_unc,
+        fmt="*",
+        markersize=14,
+        zorder=0,
+        alpha=0.8,
+        color="crimson",
+        label="average diffuse",
+    )
+
+    ax[2, 1].errorbar(
+        alpha,
+        ave_RV,
+        xerr=[[alpha_min], [alpha_plus]],
+        yerr=ave_RV_unc,
+        fmt="*",
+        markersize=14,
+        zorder=0,
+        alpha=0.8,
+        color="crimson",
+        label="average diffuse",
+    )
+
     # finalize the plot
     ax[0, 0].set_ylabel(r"$\alpha$", fontsize=fs)
     ax[1, 0].set_ylabel("$A(V)$", fontsize=fs)
@@ -551,12 +636,11 @@ def compare_AV_lit(ext_path, lit_path, plot_path, starpair_list):
     )
 
     # Gordon et al. 2021
-    G21 = pd.read_table(lit_path + "Gordon+21_tab5.dat", sep="\s+", index_col="Star")
+    G21 = pd.read_table(lit_path + "Gordon+21_tab5.dat", sep="\s+", index_col="name")
 
     # plot the literature A(V) values against the calculated values
     fig, ax = plt.subplots(figsize=(6, 6))
     for starpair in starpair_list:
-        print(starpair)
         extdata = ExtData("%s%s_ext.fits" % (ext_path, starpair.lower()))
         red_star = starpair.split("_")[0]
         if red_star in CCM89.index:
@@ -564,43 +648,57 @@ def compare_AV_lit(ext_path, lit_path, plot_path, starpair_list):
                 extdata.columns["AV"][0],
                 CCM89.loc[red_star, "A(V)"],
                 xerr=([extdata.columns["AV"][1]], [extdata.columns["AV"][2]]),
+                yerr=CCM89.loc[red_star, "A(V)"] * 0.05,
                 marker="o",
                 color="tab:blue",
                 elinewidth=1,
+                alpha=0.8,
             )
         if red_star in VCG04.index:
             ax.errorbar(
                 extdata.columns["AV"][0],
                 VCG04.loc[red_star, "A(V)"],
                 xerr=([extdata.columns["AV"][1]], [extdata.columns["AV"][2]]),
+                yerr=VCG04.loc[red_star, "e_A(V)"],
                 marker="d",
                 color="tab:orange",
                 elinewidth=1,
+                alpha=0.8,
             )
         if red_star in G09.index:
             ax.errorbar(
                 extdata.columns["AV"][0],
                 G09.loc[red_star, "AV"],
                 xerr=([extdata.columns["AV"][1]], [extdata.columns["AV"][2]]),
+                yerr=np.sqrt(
+                    G09.loc[red_star, "AV_runc"] ** 2
+                    + G09.loc[red_star, "AV_sunc"] ** 2
+                ),
                 marker="^",
                 color="tab:green",
                 elinewidth=1,
+                alpha=0.8,
             )
         if red_star.lower() in G21.index:
             ax.errorbar(
                 extdata.columns["AV"][0],
-                G21.loc[red_star.lower(), "A(V)"],
+                G21.loc[red_star.lower(), "AV"],
                 xerr=([extdata.columns["AV"][1]], [extdata.columns["AV"][2]]),
+                yerr=(
+                    [G21.loc[red_star.lower(), "AV_munc"]],
+                    [G21.loc[red_star.lower(), "AV_punc"]],
+                ),
                 marker="P",
                 color="tab:red",
                 elinewidth=1,
+                alpha=0.8,
             )
 
     # create the legend
-    handle1 = Line2D([], [], lw=0, color="tab:blue", marker="o")
-    handle2 = Line2D([], [], lw=0, color="tab:orange", marker="d")
-    handle3 = Line2D([], [], lw=0, color="tab:green", marker="^")
-    handle4 = Line2D([], [], lw=0, color="tab:red", marker="P")
+    handle1 = Line2D([], [], lw=0, color="tab:blue", marker="o", alpha=0.8)
+    handle2 = Line2D([], [], lw=0, color="tab:orange", marker="d", alpha=0.8)
+    handle3 = Line2D([], [], lw=0, color="tab:green", marker="^", alpha=0.8)
+    handle4 = Line2D([], [], lw=0, color="tab:red", marker="P", alpha=0.8)
     labels = [
         "Cardelli et al. (1989)",
         "Valencic et al. (2004)",
@@ -614,7 +712,7 @@ def compare_AV_lit(ext_path, lit_path, plot_path, starpair_list):
     ax.set_xlim(0.45, 5.8)
     ax.set_ylim(0.45, 5.8)
     ax.set_aspect("equal", adjustable="box")
-    plt.legend(handles, labels, fontsize=fs * 0.75)
+    plt.legend(handles, labels, fontsize=fs * 0.7)
     plt.xlabel(r"$A(V)$ from this work")
     plt.ylabel(r"$A(V)$ from the literature")
     plt.savefig(plot_path + "AV_comparison.pdf", bbox_inches="tight")
@@ -630,15 +728,15 @@ if __name__ == "__main__":
     diffuse = [
         "BD+56d524_HD034816",
         "HD013338_HD031726",
-        "HD014250_HD042560",
+        "HD014250_HD031726",
         "HD014422_HD214680",
-        "HD014956_HD188209",
+        "HD014956_HD214680",
         "HD017505_HD214680",
         "HD029309_HD042560",
-        # "HD034921_HD214680",
+        "HD034921_HD214680",
         "HD037020_HD034816",
         "HD037022_HD034816",
-        "HD037023_HD034816",
+        "HD037023_HD036512",
         "HD037061_HD034816",
         "HD038087_HD051283",
         "HD052721_HD091316",
@@ -646,39 +744,39 @@ if __name__ == "__main__":
         "HD166734_HD031726",
         "HD183143_HD188209",
         "HD185418_HD034816",
-        "HD192660_HD204172",
+        "HD192660_HD214680",
         "HD204827_HD003360",
-        "HD206773_HD003360",
+        "HD206773_HD047839",
         "HD229238_HD214680",
-        "HD294264_HD034759",
+        "HD294264_HD051283",
     ]
 
-    dense = ["HD029647_HD042560", "HD283809_HD003360"]
+    dense = ["HD029647_HD034759", "HD283809_HD003360"]
 
     flagged = [
-        "HD014250_HD042560",
+        "HD014250_HD031726",
         "HD014422_HD214680",
         "HD034921_HD214680",
         "HD037020_HD034816",
         "HD037022_HD034816",
-        "HD037023_HD034816",
+        "HD037023_HD036512",
         "HD052721_HD091316",
         "HD166734_HD031726",
-        "HD206773_HD003360",
-        "HD294264_HD034759",
+        "HD206773_HD047839",
+        "HD294264_HD051283",
     ]
 
-    # subtract the flagged stars from the star pair list
+    # subtract the flagged stars from the star pair lists
     good_diffuse = list((Counter(diffuse) - Counter(flagged)).elements())
     good_dense = list((Counter(dense) - Counter(flagged)).elements())
 
     # settings for the plotting
-    fs = 18
+    fs = 20
     plt.rc("font", size=fs)
-    plt.rc("xtick", direction="in", labelsize=fs * 0.8)
-    plt.rc("ytick", direction="in", labelsize=fs * 0.8)
-    plt.rc("xtick.major", width=1, size=8)
-    plt.rc("ytick.major", width=1, size=8)
+    plt.rc("xtick.major", width=1, size=10)
+    plt.rc("ytick.major", width=1, size=10)
+    plt.rc("xtick", top=True, direction="in", labelsize=fs * 0.8)
+    plt.rc("ytick", right=True, direction="in", labelsize=fs * 0.8)
 
     # create tables
     # table_results(inpath, table_path, good_diffuse, good_dense)
